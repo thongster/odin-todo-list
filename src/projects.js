186d218c73479c963e5fd92209a16ac6d42b766a
thongster <106802301+thongster@users.noreply.github.com>
2025-10-29 14:19:26 +0700

    logic for display project

--- src/projects.js
+++ src/projects.js
@@ -1,22 +1,40 @@
+const projectList = []
+
 class Project {
     constructor(name) {
         this.name = name;
-        this.activeTasks;
-        this.completedTasks;
+        this.activeTasks = 0;
+        this.completedTasks = 0;
+    }
+
+    addToProjectList() {
+        projectList.push(this);
     }
 }
 
-
-
-function displayProject () {
-    
+function displayProject(projectName) {
+    const projectList = document.querySelector(".projectList")
+    const projectButton = document.createElement("button")
+    projectButton.classList.add("buttonOne", "projectButton")
+    projectButton.textContent = projectName
+    projectList.append(projectButton)
 }
 
 function addProject() {
-    const exampleProject = new Project("im in this bitch again")
-    console.log(exampleProject.name)
-    console.log(exampleProject.activeTasks)
-    console.log(exampleProject.completedTasks)
+    const exampleProject = new Project("project example 1")
+    const exampleProject2 = new Project("project example 2")
+    const exampleProject3 = new Project("project example 3")
+    const exampleProject4 = new Project("project example 4")
+
+    exampleProject.addToProjectList()
+    exampleProject2.addToProjectList()
+    exampleProject3.addToProjectList()
+    exampleProject4.addToProjectList()
+    console.log(projectList)
+
+    projectList.forEach((e) => {
+        displayProject(e.name)
+    })
 }
 
 export {addProject, displayProject}
