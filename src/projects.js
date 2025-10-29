172d7cdb8df812cfd9c09317b19c23648e6ff590
thongster <106802301+thongster@users.noreply.github.com>
2025-10-29 15:03:21 +0700

    created modal in html/css

--- src/projects.js
+++ src/projects.js
@@ -3,15 +3,16 @@ const projectList = []
 class Project {
     constructor(name) {
         this.name = name;
-        this.activeTasks = 0;
-        this.completedTasks = 0;
+        this.activeTasks = 0
+        this.completedTasks = 0
     }
 
     addToProjectList() {
-        projectList.push(this);
+        projectList.push(this)
     }
 }
 
+// take project name, display on dom as button
 function displayProject(projectName) {
     const projectList = document.querySelector(".projectList")
     const projectButton = document.createElement("button")
@@ -21,6 +22,11 @@ function displayProject(projectName) {
 }
 
 function addProject() {
+    const newProjectButton = document.querySelector("#newProjectButton")
+    newProjectButton.addEventListener("click", (e) => {
+        e.preventDefault
+        alert("it works")
+    })
     const exampleProject = new Project("project example 1")
     const exampleProject2 = new Project("project example 2")
     const exampleProject3 = new Project("project example 3")
