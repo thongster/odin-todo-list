6cb8b83e5b0a1c3d20cbe13a7091a7dff5aabef6
thongster <106802301+thongster@users.noreply.github.com>
2025-10-31 13:00:27 +0700

    display completed tasks to dom

--- src/activeTaskDisplay.js
+++ src/activeTaskDisplay.js
@@ -19,60 +19,19 @@ const activeTasksControl = function(currentProj) {
     function displayNewTask() {
         // if tasks exist, remove the no task box
         // if tasks dont exist, show it and don't continue with function
-        if (projectToShow.activeTasks.length > 0) {
+        if (projectToShow.activeTasks.length > 0 || projectToShow.completedTasks.length > 0) {
             removeNoTasksBox()
-        } else if (projectToShow.activeTasks.length === 0) {
+        } else if (projectToShow.activeTasks.length === 0 && projectToShow.completedTasks.length === 0) {
             showNoTasksBox()
             return;
         }
+
+        // display active and completed tasks
         for (let i = 0; i < projectToShow.activeTasks.length; i++) {
-            
-            const activeTaskTitle = document.createElement("h2")
-            const activeTaskItem = document.createElement("div")
-            const buttonDiv = document.createElement("div")
-            const checkTaskButton = document.createElement("button")
-            const taskMiddleSection = document.createElement("div")
-            const taskText = document.createElement("div")
-            const taskTitle = document.createElement("h3")
-            const taskDesc = document.createElement("p")
-            const taskInfo = document.createElement("div")
-            const taskPriority = document.createElement("button")
-            const taskProject = document.createElement("button")
-            const taskDueDate = document.createElement("button")
-            const taskOptions = document.createElement("div")
-            const taskEdit = document.createElement("button")
-            const taskDelete = document.createElement("button")
-            
-            activeTaskItem.classList.add("activeTaskItem")
-            activeTaskTitle.id = "activeTaskTitle"
-            buttonDiv.classList.add("buttonDiv")
-            taskMiddleSection.classList.add("taskMiddleSection")
-            taskText.classList.add("taskText")
-            taskInfo.classList.add("taskInfo")
-            taskOptions.classList.add("taskOptions")
-
-            // full in task based on index of activeTasks
-            activeTaskTitle.textContent = "Active Tasks"
-            taskTitle.textContent = projectToShow.activeTasks[i].title
-            taskDesc.textContent = projectToShow.activeTasks[i].shortDesc
-            taskPriority.textContent = projectToShow.activeTasks[i].priority
-            taskProject.textContent = projectToShow.activeTasks[i].project
-            taskDueDate.textContent = projectToShow.activeTasks[i].dueDate
-            taskEdit.textContent = "Edit"
-            taskDelete.textContent = "Delete"
-            
-            // if title doesn't exist yet, then append it
-            if (i === 0) {
-                activeTaskBox.append(activeTaskTitle)
-            }
-
-            activeTaskBox.append(activeTaskItem)
-            activeTaskItem.append(buttonDiv, taskMiddleSection, taskOptions)
-            buttonDiv.append(checkTaskButton)
-            taskMiddleSection.append(taskText, taskInfo)
-            taskText.append(taskTitle, taskDesc)
-            taskInfo.append(taskPriority, taskProject, taskDueDate)
-            taskOptions.append(taskEdit, taskDelete)        
+            displayActiveTasks(projectToShow, i)
+        }
+        for (let i = 0; i < projectToShow.completedTasks.length; i++) {
+            displayCompletedTasks(projectToShow, i)
         }
     }
 
@@ -95,6 +54,106 @@ const activeTasksControl = function(currentProj) {
 
     
     return {displayNewTask, hideDisplayNewTask, removeNoTasksBox, showNoTasksBox}
+}
+
+function displayActiveTasks(projectToShow, i) {
+    const activeTaskBox = document.querySelector(".activeTaskBox")
+    const activeTaskTitle = document.createElement("h2")
+    const activeTaskItem = document.createElement("div")
+    const buttonDiv = document.createElement("div")
+    const checkTaskButton = document.createElement("button")
+    const taskMiddleSection = document.createElement("div")
+    const taskText = document.createElement("div")
+    const taskTitle = document.createElement("h3")
+    const taskDesc = document.createElement("p")
+    const taskInfo = document.createElement("div")
+    const taskPriority = document.createElement("button")
+    const taskProject = document.createElement("button")
+    const taskDueDate = document.createElement("button")
+    const taskOptions = document.createElement("div")
+    const taskEdit = document.createElement("button")
+    const taskDelete = document.createElement("button")
+    
+    activeTaskItem.classList.add("activeTaskItem")
+    activeTaskTitle.id = "activeTaskTitle"
+    buttonDiv.classList.add("buttonDiv")
+    taskMiddleSection.classList.add("taskMiddleSection")
+    taskText.classList.add("taskText")
+    taskInfo.classList.add("taskInfo")
+    taskOptions.classList.add("taskOptions")
+
+    // full in task based on index of activeTasks
+    activeTaskTitle.textContent = "Active Tasks"
+    taskTitle.textContent = projectToShow.activeTasks[i].title
+    taskDesc.textContent = projectToShow.activeTasks[i].shortDesc
+    taskPriority.textContent = projectToShow.activeTasks[i].priority
+    taskProject.textContent = projectToShow.activeTasks[i].project
+    taskDueDate.textContent = projectToShow.activeTasks[i].dueDate
+    taskEdit.textContent = "Edit"
+    taskDelete.textContent = "Delete"
+    
+    // if title doesn't exist yet, then append it
+    if (i === 0) {
+        activeTaskBox.append(activeTaskTitle)
+    }
+
+    activeTaskBox.append(activeTaskItem)
+    activeTaskItem.append(buttonDiv, taskMiddleSection, taskOptions)
+    buttonDiv.append(checkTaskButton)
+    taskMiddleSection.append(taskText, taskInfo)
+    taskText.append(taskTitle, taskDesc)
+    taskInfo.append(taskPriority, taskProject, taskDueDate)
+    taskOptions.append(taskEdit, taskDelete)    
+}
+
+function displayCompletedTasks(projectToShow, i) {
+    const completedTaskBox = document.querySelector(".completedTaskBox")
+    const completedTaskTitle = document.createElement("h2")
+    const completedTaskItem = document.createElement("div")
+    const buttonDiv = document.createElement("div")
+    const checkTaskButton = document.createElement("button")
+    const taskMiddleSection = document.createElement("div")
+    const taskText = document.createElement("div")
+    const taskTitle = document.createElement("h3")
+    const taskDesc = document.createElement("p")
+    const taskInfo = document.createElement("div")
+    const taskPriority = document.createElement("button")
+    const taskProject = document.createElement("button")
+    const taskDueDate = document.createElement("button")
+    const taskOptions = document.createElement("div")
+    const taskEdit = document.createElement("button")
+    const taskDelete = document.createElement("button")
+    
+    completedTaskItem.classList.add("completedTaskItem")
+    completedTaskTitle.id = "completedTaskTitle"
+    buttonDiv.classList.add("buttonDiv")
+    taskMiddleSection.classList.add("taskMiddleSection")
+    taskText.classList.add("taskText")
+    taskInfo.classList.add("taskInfo")
+    taskOptions.classList.add("taskOptions")
+
+    // full in task based on index of activeTasks
+    completedTaskTitle.textContent = "Completed Tasks"
+    taskTitle.textContent = projectToShow.completedTasks[i].title
+    taskDesc.textContent = projectToShow.completedTasks[i].shortDesc
+    taskPriority.textContent = projectToShow.completedTasks[i].priority
+    taskProject.textContent = projectToShow.completedTasks[i].project
+    taskDueDate.textContent = projectToShow.completedTasks[i].dueDate
+    taskEdit.textContent = "Edit"
+    taskDelete.textContent = "Delete"
+    
+    // if title doesn't exist yet, then append it
+    if (i === 0) {
+        completedTaskBox.append(completedTaskTitle)
+    }
+
+    completedTaskBox.append(completedTaskItem)
+    completedTaskItem.append(buttonDiv, taskMiddleSection, taskOptions)
+    buttonDiv.append(checkTaskButton)
+    taskMiddleSection.append(taskText, taskInfo)
+    taskText.append(taskTitle, taskDesc)
+    taskInfo.append(taskPriority, taskProject, taskDueDate)
+    taskOptions.append(taskEdit, taskDelete)    
 }
 
 function hideActiveTasks() {
